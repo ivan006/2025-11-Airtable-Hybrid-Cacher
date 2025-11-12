856725ee767501018e164095418255d0c166bfba
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-11-12 16:16:52 +0200

    1

--- bound-cache.php
+++ bound-cache.php
@@ -1,4 +1,14 @@
 <?php
+// 🌍 Allow all origins and handle preflight
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(204);
+  exit;
+}
+
 header('Content-Type: application/json; charset=utf-8');
 require 'helpers.php';
 
